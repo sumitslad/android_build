916dccb74be24ba576e0f14af3b34563409b315d
Ricardo Salveti de Araujo <rsalveti@rsalveti.net>
2014-11-11 19:08:30 -0200

    Changing series to vivid
    Change-Id: Ic59d729c0b851e72e854718b8861d66ee5d5506a
    Signed-off-by: Ricardo Salveti de Araujo <rsalveti@rsalveti.net>

--- target/board/generic/BoardConfig.mk
+++ target/board/generic/BoardConfig.mk
@@ -81,4 +81,4 @@ BOARD_SEPOLICY_UNION += domain.te surfaceflinger.te
 
 TARGET_KERNEL_UBUNTU := true
 TARGET_KERNEL_UBUNTU_META := linux-image-goldfish
-TARGET_KERNEL_UBUNTU_SERIES := utopic
+TARGET_KERNEL_UBUNTU_SERIES := vivid
